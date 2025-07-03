7edd37b25097b1dc8704e0e4523aab8ce20b4dcc
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-03 22:54:21 +0600

    update: install cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import { bookRoutes } from "./app/controllers/book.controller";
 import borrowRoutes from "./app/controllers/borrow.controller";
+import cors from "cors";
+
 
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
