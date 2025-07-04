cb3f638d243864d9e5da2f62ca3d6da8cd6bcba0
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-04 12:22:24 +0600

    Update CORS settings

--- src/app.ts
+++ src/app.ts
@@ -7,19 +7,7 @@ import cors from "cors";
 
 const app: Application = express();
 
-const allowedOrigins = ["http://localhost:5173", "https://l2-a4-client.vercel.app"];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    }
-  })
-);
+  app.use(cors());
 
 
 app.use(express.json());
