5ac93dfe27ba8abbcf3b6a6c0eac6b61b3a88fca
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-07-04 10:38:25 +0600

    update: cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import cors from "cors";
 
 
 const app: Application = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
